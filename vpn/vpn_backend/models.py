75851db93d000864b3d8fa00fe4959a8e907f9dc
DragonSasha <51650290+DragonSasha@users.noreply.github.com>
2023-11-13 18:50:56 +0200

    added models

--- vpn/vpn_backend/models.py
+++ vpn/vpn_backend/models.py
@@ -1,7 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-from datetime import datetime
+
+class Website(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    url = models.CharField(max_length=500)
 
 
-
+# TODO: potentialy allow storage of data per x days for better tracking
+class Statistics(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.OneToOneField(Website, on_delete=models.CASCADE)
+    page_views = models.IntegerField(default=0, null=False, validators=[models.MinValueValidator(0)])
