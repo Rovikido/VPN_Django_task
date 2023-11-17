504a5c1506448a74ce861b15c4adfc3433ddc3c7
DragonSasha <51650290+DragonSasha@users.noreply.github.com>
2023-11-17 02:43:00 +0200

    added view urls

--- vpn/vpn/urls.py
+++ vpn/vpn/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib import admin
 from vpn_backend.views import (
     UserRegistrationView,
     UserLoginView,
@@ -10,16 +11,27 @@ from vpn_backend.views import (
     WebsiteCreateView,
     VPNView,
 )
+from vpn_backend.views_alternative import *
 
 
 urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='user-register'),
-    path('login/', UserLoginView.as_view(), name='user-login'),
-    path('logout/', UserLogoutView.as_view(), name='user-login'),
-    path('update-user/', UserUpdateView.as_view(), name='user-update'),
-    path('statistics/', StatisticsListView.as_view(), name='statistics-list'),
-    path('websites/', WebsiteListView.as_view(), name='website-list'),
-    path('websites/<int:pk>/', WebsiteDetailView.as_view(), name='website-detail'),
-    path('websites/create/', WebsiteCreateView.as_view(), name='create-website'),
+    # path('admin/', admin.site.urls),
+    # path('api/register/', UserRegistrationView.as_view(), name='api-user-register'),
+    # path('api/login/', UserLoginView.as_view(), name='api-user-login'),
+    # path('api/logout/', UserLogoutView.as_view(), name='api-user-logout'),
+    # path('api/update-user/', UserUpdateView.as_view(), name='api-user-update'),
+    # path('api/statistics/', StatisticsListView.as_view(), name='api-statistics-list'),
+    # path('api/websites/', WebsiteListView.as_view(), name='api-website-list'),
+    # path('api/websites/<int:pk>/', WebsiteDetailView.as_view(), name='api-website-detail'),
+    # path('api/websites/create/', WebsiteCreateView.as_view(), name='api-create-website'),
     path('vpn/<path:user_site>/', VPNView.as_view(), name='vpn-view'),
+
+    path('register/', UserRegistrationViewAlternative.as_view(), name='user-register'),
+    path('login/', UserLoginViewAlternative.as_view(), name='user-login'),
+    path('logout/', UserLogoutViewAlternative.as_view(), name='user-logout'),
+    path('update-user/', UserUpdateViewAlternative.as_view(), name='user-update'),
+    path('statistics/', StatisticsListViewAlternative.as_view(), name='statistics-list'),
+    path('websites/', WebsiteListViewAlternative.as_view(), name='website-list'),
+    path('websites/<int:pk>/', WebsiteDetailViewAlternative.as_view(), name='website-detail'),
+    path('websites/create/', WebsiteCreateViewAlternative.as_view(), name='create-website'),
 ]
