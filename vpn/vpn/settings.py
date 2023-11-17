48490bc307444406ab34e49bf8f694f9ef3462a2
DragonSasha <51650290+DragonSasha@users.noreply.github.com>
2023-11-17 02:42:49 +0200

    added path to statics

--- vpn/vpn/settings.py
+++ vpn/vpn/settings.py
@@ -41,6 +41,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.TemplateHTMLRenderer',
+    ],
+}
+
+STATIC_URL = 'static/'  
+STATICFILES_DIRS = [
+    "vpn/static"
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -117,8 +129,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
